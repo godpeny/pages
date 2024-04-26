eb7cda2310dbece514bde2565b2d0549137ef5f0
godpeny <slsnsepdpd@gmail.com>
2024-04-26 21:27:50 +0900

    chore: [GSMSEPLC-29] Make new Topic "Lagrange Duality" from Support Vector Machines (SVM)" ---  - Make new Topic "Lagrange Duality" from Support Vector Machines (SVM)".

--- main/src/Blog.tsx
+++ main/src/Blog.tsx
@@ -26,7 +26,7 @@ import post9 from './blog-post.9.md';
 import post10 from './blog-post.10.md';
 import post11 from './blog-post.11.md';
 import post12 from './blog-post.12.md';
-
+import post13 from './blog-post.13.md';
 
 const sections = [
   { title: 'Technology', url: '#' },
@@ -69,7 +69,7 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post0,post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11, post12];
+const posts = [post0,post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11, post12, post13];
 
 const sidebar = {
   title: 'About',
